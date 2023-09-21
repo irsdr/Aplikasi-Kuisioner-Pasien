575e593de0a6f2aab23297d2e5b5fe3dd15216af
Muh. Irsyad Asrori <sastrojendro1119@gmail.com>
2023-09-21 20:25:17 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://sstr.rf.gd/';
+$config['base_url'] = 'https://aplikasi-kuisioner-pasien-wzmpjprosq-et.a.run.app/';
 
 /*
 |--------------------------------------------------------------------------
